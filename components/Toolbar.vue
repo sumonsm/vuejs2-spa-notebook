64222463f5985a3d0ee69e2f6b154930c6441528
Sumon SM <sumonsm00@gmail.com>
2018-04-23 21:19:40 +0600

    finalized vue.js 2 notbook application

---
+++ components/Toolbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="toolbar">
+    <span class="title">Vue Notebook //</span>
+    <span>
+      <i @click="addNote" class="glyphicon glyphicon-plus">New</i>
+      <i @click="toggleFavorite"
+        class="glyphicon glyphicon-pushpin"
+        :class="{starred: activeNote.favorite}">Pin</i>
+      <i @click="deleteNote" class="glyphicon glyphicon-remove">Delete</i>
+    </span>
+  </div>
+</template>
+
+<script>
+  import { addNote, deleteNote, toggleFavorite } from '../vuex/actions'
+
+  export default {
+    vuex: {
+      getters: {
+        activeNote: state => state.activeNote
+      },
+      actions: {
+        addNote,
+        deleteNote,
+        toggleFavorite
+      }
+    }
+  }
+</script>
